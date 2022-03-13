2791ec38ce95dddd918df0e09450557aee60ba94
OnlyDropped <asheburenkov@yandex.ru>
2022-03-14 00:48:35 +0300

    added header, footeк and slider

---
+++ js/app.js
@@ -0,0 +1,5 @@
+$('.slider-content').slick({
+  arrows: true,
+  prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/arrow-prev.svg" alt=""></button>',
+  nextArrow: '<button type="button" class="slick-next"><img src="img/slider/arrow-prev.svg" alt=""></button>',
+});
