73445d7393b48d64f22a0703ee4df19c32a90282
OnlyDropped <asheburenkov@yandex.ru>
2022-04-04 22:36:01 +0300

    added new page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,35 @@
+$(".catalog-filter__text").ionRangeSlider({
+  type: "double",
+  min: 0,
+  max: 120000,
+  from: 2000,
+  to: 102000,
+  prefix: "₽",
+  onStart: function (data){
+    $('.catalog-filter__price-from').text(data.from);
+    $('.catalog-filter__price-to').text(data.to);
+  },
+  onChange: function (data) {
+    $('.catalog-filter__price-from').text(data.from);
+    $('.catalog-filter__price-to').text(data.to);
+  },
+});
+
+$('.catalog-filter__select, .catalog-content__select').styler();
+
 $('.slider-content').slick({
   arrows: true,
   prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/arrow-prev.svg" alt=""></button>',
   nextArrow: '<button type="button" class="slick-next"><img src="img/slider/arrow-prev.svg" alt=""></button>',
 });
+
+
+
+$('.star').rateYo({
+  starWidth: "15px",
+  normalFill: "#000",
+  numStars: 5,
+
+});  
+
+
